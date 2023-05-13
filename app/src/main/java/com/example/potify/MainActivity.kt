3db2652efcc7b5dab8881f04f537c6245531df8f
p.dehghan <dev.poorya.db@gmail.com>
2023-05-13 09:06:20 +0330

    fixes findNavController usage

--- app/src/main/java/com/example/potify/MainActivity.kt
+++ app/src/main/java/com/example/potify/MainActivity.kt
@@ -1,25 +1,24 @@
 package com.example.potify
 
-import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.potify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater,null,false)
+        setContentView(binding!!.root)
 
-        val binding: ActivityMainBinding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_main, null, false)
-
-        val navController = findNavController(this, R.id.myNavHostFragment)
+        val navController = findNavController(R.id.myNavHostFragment)
         //NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
-        binding.bottomNavigationView.setupWithNavController(navController)
+        binding?.bottomNavigationView?.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
